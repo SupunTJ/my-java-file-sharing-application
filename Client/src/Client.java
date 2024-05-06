988224b7f8a5f26d8d3ac3978971934fa790f8b7
SupunTJ <jayaweeraspn@gmail.com>
2024-05-06 14:20:45 +0530

    making the client UI

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,5 +1,38 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.io.File;
+
 public class Client {
     public static void main(String[] args) {
-        
+        File fileToSend;
+
+        JFrame jFrame = new JFrame("File Sharing Client");
+        jFrame.setSize(450,450);
+        jFrame.setLayout(new BoxLayout(jFrame.getContentPane(),BoxLayout.Y_AXIS));
+        jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
+
+        JLabel jlTitle = new JLabel("File Sender Window");
+        jlTitle.setFont(new Font("Arial", Font.BOLD, 25));
+        jlTitle.setBorder(new EmptyBorder(20,0,10,0));
+        jlTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JLabel jlFileName = new JLabel("File Sender Window");
+        jlFileName.setFont(new Font("Arial", Font.BOLD, 20));
+        jlFileName.setBorder(new EmptyBorder(50,0,10,0));
+        jlFileName.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JPanel jpButton = new JPanel();
+        jpButton.setBorder(new EmptyBorder(75,0,10,0));
+
+        JButton jbSendFile = new JButton("Send File");
+        jbSendFile.setPreferredSize(new Dimension(150, 75));
+        jbSendFile.setFont(new Font("Arial", Font.BOLD, 20));
+
+        JButton jbChooseFile = new JButton("Choose File");
+        jbChooseFile.setPreferredSize(new Dimension(150, 75));
+        jbChooseFile.setFont(new Font("Arial", Font.BOLD, 20));
+
+
     }
 }
