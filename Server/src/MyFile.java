c2ae429c9589c1958f201216eac891056237db32
SupunTJ <jayaweeraspn@gmail.com>
2024-05-06 15:46:14 +0530

    Server UI Completed

--- Server/src/MyFile.java
+++ Server/src/MyFile.java
@@ -1,2 +1,45 @@
 public class MyFile {
+    private String id;
+    private String name;
+    private byte[] data;
+    private String fileExtension;
+
+    public MyFile(String id, String name, byte[] data, String fileExtension) {
+        this.id = id;
+        this.name = name;
+        this.data = data;
+        this.fileExtension = fileExtension;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public void setFileExtension(String fileExtension) {
+        this.fileExtension = fileExtension;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public String getFileExtension() {
+        return fileExtension;
+    }
 }
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,2 +1,33 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Server {
+
+    static ArrayList<MyFile> myFiles = new ArrayList<>();
+    public static void main(String[] args) {
+        int fileId = 0;
+
+        JFrame jFrame = new JFrame("File Sharing Server");
+        jFrame.setSize(400,400);
+        jFrame.setLayout(new BoxLayout(jFrame.getContentPane(),BoxLayout.Y_AXIS));
+        jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
+
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.Y_AXIS));
+
+        JScrollPane jScrollPane = new JScrollPane();
+        jScrollPane.setVerticalScrollBarPolicy(jScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        JLabel jlTitle = new JLabel("File Receiver ");
+        jlTitle.setFont(new Font("Arial", Font.BOLD, 25));
+        jlTitle.setBorder(new EmptyBorder(20,0,10,0));
+        jlTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        jFrame.add(jlTitle);
+        jFrame.add(jScrollPane);
+        jFrame.setVisible(true);
+
+    }
 }
